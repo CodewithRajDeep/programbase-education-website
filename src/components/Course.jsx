d091fac1f437dd41f3b52fba111f37f074077fc0
Deep Raj <deepraj6897@gmail.com>
2024-06-22 12:34:30 +0530

    added frontend

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import Cards from "./Cards";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Cards from "./Cards";
 function Course() {
   const [book, setBook] = useState([]);
   useEffect(() => {
@@ -21,17 +21,11 @@ function Course() {
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div className="mt-28 items-center justify-center text-center">
           <h1 className="text-2xl  md:text-4xl">
-            We're delighted to have you{" "}
+            We're delighted to have you!!
             <span className="text-pink-500"> Here! :)</span>
           </h1>
           <p className="mt-12">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
-            assumenda? Repellendus, iste corrupti? Tempore laudantium
-            repellendus accusamus accusantium sed architecto odio, nisi expedita
-            quas quidem nesciunt debitis dolore non aspernatur praesentium
-            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
-            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
-            consequatur!
+           want to learn something new, enroll yourself and enjoy the ride
           </p>
           <Link to="/">
             <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
